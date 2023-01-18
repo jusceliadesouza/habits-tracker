7624e142bd5f17b021afbfaf9bea063cc1145405
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-01-18 13:56:54 -0300

    listagem dos hábitos

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,18 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get("/hello", () => {
-  return "Hello NLW";
+app.get("/hello", async () => {
+  const habits = await prisma.habit.findMany();
+  return habits;
 });
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log("HTTP Server running")
-});
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server running");
+  });
