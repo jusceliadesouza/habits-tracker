6006bdd4a1dc893fd5c5a41966b92403dcc01afa
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-01-20 16:10:36 -0300

    web: componente header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,14 @@
-import { Habit } from "./components/Habit";
 
-function App() {
+// import { Habit } from "./components/Habit";
+
+import { Header } from "./components/Header";
+
+export function App() {
   return (
-    <Habit completed={3}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
+    </div>
   );
 }
-
-export default App;
