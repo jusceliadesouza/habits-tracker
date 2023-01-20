3eda016741fcfd82bf718476fe7500bea990cbb3
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-01-20 14:30:35 -0300

    server: rota detalhe do dia / habitos completos e possíveis

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -13,20 +13,67 @@ export async function appRoutes(app: FastifyInstance) {
 
     const { title, weekDays } = createHabitBody.parse(request.body);
 
-    const today = dayjs().startOf('day').toDate() // 
+    const today = dayjs().startOf("day").toDate(); //
 
     await prisma.habit.create({
       data: {
         title,
         created_at: new Date(),
         weekDays: {
-          create: weekDays.map(weekDay => {
+          create: weekDays.map((weekDay) => {
             return {
               week_day: weekDay,
-            }
-          })
-        }
-      }
-    })
+            };
+          }),
+        },
+      },
+    });
+  });
+
+  app.get("/day", async (request) => {
+    const getDaysParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDaysParams.parse(request.query);
+
+    const parsedDate = dayjs(date).startOf("day");
+    const weekDay = parsedDate.get("day");
+
+    // console.log(date, weekDay)
+
+    // todos os hábitos possíveis
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
+
+    // todos os hábitos completados
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
   });
 }
