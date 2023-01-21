191675bde46bda55d985367ad466b4dd7f27195f
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-01-21 11:53:31 -0300

    mobile: navegação da tela de cadastro e tipagem das rotas

---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      hone: undefined;
+      new: undefined;
+      habit: {
+        date: string; 
+      };
+    }
+  }
+}
