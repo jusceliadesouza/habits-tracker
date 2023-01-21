6a9761a482fc28b046756e867b72fe3f75acc691
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-01-21 15:16:17 -0300

    mobile: botão confirmar criação de hábito

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
-import { ScrollView, Text, TextInput, View } from "react-native";
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import colors from "tailwindcss/colors";
 
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
@@ -18,16 +26,18 @@ export function New() {
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
   function handleToogleWeekDays(weekDayIndex: number) {
-    if(weekDays.includes(weekDayIndex)) {
-      setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex))
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays((prevState) =>
+        prevState.filter((weekDay) => weekDay !== weekDayIndex)
+      );
     } else {
-      setWeekDays(prevState => [...prevState, weekDayIndex])
+      setWeekDays((prevState) => [...prevState, weekDayIndex]);
     }
   }
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 50}}>
         <BackButton />
 
         <Text className="mt-6 text-white font-extrabold text-3xl">
@@ -38,22 +48,35 @@ export function New() {
           Qual seu comprometimento?
         </Text>
 
-        <TextInput className="h-12 pl-3 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600" />
+        <TextInput
+          className="h-12 pl-3 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
+          placeholder="Exercícios, dormir bem, etc..."
+          placeholderTextColor={colors.zinc[400]}
+        />
 
         <Text className="mt-4 mb-3 text-white font-semibold text-base">
           Qual a recorrência?
         </Text>
 
-        {
-          availableWeekDays.map((weekDay, index) => (
-            <Checkbox 
-              key={weekDay} 
-              title={weekDay}
-              checked={weekDays.includes(index)}
-              onPress={() => handleToogleWeekDays(index)}
-            />
-          ))
-        }
+        {availableWeekDays.map((weekDay, index) => (
+          <Checkbox
+            key={weekDay}
+            title={weekDay}
+            checked={weekDays.includes(index)}
+            onPress={() => handleToogleWeekDays(index)}
+          />
+        ))}
+
+        <TouchableOpacity
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+        >
+          <Feather name="check" size={20} color={colors.white} />
+
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirmar
+          </Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
