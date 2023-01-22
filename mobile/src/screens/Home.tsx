80ff9b2bcb8edec0fb91e89e8e4bf5c373328340
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-01-22 17:43:28 -0300

    mobile: interface de hábitos do dia

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -4,6 +4,7 @@ import { generateRangeDatesFromYearStart } from "../utils/generate-dates-from-ye
 
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const dateFromYearStart = generateRangeDatesFromYearStart();
@@ -11,6 +12,8 @@ const minimumSummaryDatesSizes = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSizes - dateFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -26,30 +29,29 @@ export function Home() {
           </Text>
         ))}
       </View>
-        
-      <ScrollView 
-        showsVerticalScrollIndicator={false} 
-        contentContainerStyle={{paddingBottom: 50}}
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
       >
         <View className="flex-row flex-wrap">
           {dateFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate("habit", { date: date.toISOString() })}
+            />
           ))}
-          
-          {
-            amountOfDaysToFill > 0 && Array
-            .from({ length: amountOfDaysToFill })
-            .map((_, index) => (
-              <View 
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
                 key={index}
                 className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
-                style={{ width: DAY_SIZE, height: DAY_SIZE}}
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
               />
-            ))      
-          }         
+            ))}
         </View>
       </ScrollView>
-
     </View>
   );
 }
